02de149a5c7fab65333cee45d93df059b48400b7
SN_Subha <126484459+Subhalaxmi2004@users.noreply.github.com>
2024-12-29 09:50:01 +0530

    Button fixed

--- src/components/ClubDetail/Projects.tsx
+++ src/components/ClubDetail/Projects.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import ProjectCard from '@/components/projects/ProjectCard';
 import { sampleProjects } from '@/constants';
 import Typography from '@mui/material/Typography';
-import NewProject from './NewProject';
+import NewProject from '@/components/ClubDetail/NewProject';
+import {  TextField, Button, Box } from "@mui/material";
 const Projects: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [searchQuery, setSearchQuery] = useState<string>('');
@@ -19,161 +20,163 @@ const Projects: React.FC = () => {
     currentPage * itemsPerPage
   );
 
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
+  const handlePageChange = (page: number) => setCurrentPage(page);
   const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      handlePageChange(currentPage + 1);
-    }
+    if (currentPage < totalPages) handlePageChange(currentPage + 1);
   };
-
-  const isNextButtonDisabled = currentPage === totalPages || totalPages === 0;
 
   return (
-    <div style={styles.container}>
-      <NewProject/>
-      <h1 style={{ color: '#fff', textAlign: 'left', fontSize: '30px', marginLeft: "18px", marginTop: "18px" , fontFamily: 'Poppins , sans-serif' , fontWeight: 600}}>All Projects</h1>
-      <div style={styles.searchContainer}>
-      <Typography variant="h6" component="div" sx={{ fontWeight: 600,position:"absolute",left:"20%",top:"0",marginTop:"12px"}}>
-        SEARCH PROJECTS
+    <Box
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#1E1E1E',
+        paddingBottom: '20px',
+        fontFamily: 'Poppins, sans-serif',
+      }}
+    >
+      <NewProject />
+      <Typography
+        sx={{
+          color: '#fff',
+          textAlign: 'left',
+          fontSize: '30px',
+          marginLeft: '18px',
+          marginTop: '18px',
+          fontWeight: 600,
+        }}
+      >
+        All Projects
+      </Typography>
+      <Box
+        sx={{
+          width: {lg:"70%",md:"80%",sm:"85%",xs:"90%"},
+          height: '134px',
+          // paddingTop: '33px',
+          display: "flex",
+          flexDirection:{sm:"column",md:"column",lg:"row",xs:"column"},
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          marginBottom: '30px',
+          marginTop: '30px',
+          border: '1.5px solid #252525',
+          position: 'relative',
+          borderRadius: '4px',
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: '18px',
+            fontWeight: 600,
+            color: '#fff',
+          }}
+        >
+          SEARCH PROJECTS
         </Typography>
-        <input
-          type="text"
-          placeholder="Search in projects..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          style={styles.searchInput}
-        />
-      </div>
-      
-      <div style={styles.cardContainer}>
+        <TextField
+  variant="outlined"
+  placeholder="Search in projects..."
+  value={searchQuery}
+  onChange={(e) => setSearchQuery(e.target.value)}
+  sx={{
+    width: '60%',
+    '& .MuiOutlinedInput-root': {
+      borderRadius: '5px',
+      backgroundColor: '#1E1E1E',
+      color: '#fff',
+    },
+    '& .MuiOutlinedInput-input': {
+      padding: '10px',
+      fontSize: '16px',
+    },
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#252525',
+    },
+    '&:hover .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#555',
+    },
+    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#28a745',
+    },
+  }}
+/>
+      </Box>
+
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-around',
+          flexWrap: 'wrap',
+          margin: '0 auto',
+        }}
+      >
         {currentProjects.length > 0 ? (
           currentProjects.map((project, index) => (
-            <ProjectCard
-              key={index}
-              project={project}
-{/*               style={styles.card} */}
-            />
+            <ProjectCard key={index} project={project} />
           ))
         ) : (
-          <p style={{ color: '#fff' }}>No projects available.</p>
+          <Typography style={{ color: '#fff' }}>No projects available.</Typography>
         )}
-      </div>
+      </Box>
 
-  
       {filteredProjects.length > 0 && (
-        <div style={styles.pagination}>
-          <div style={styles.pageNumbers}>
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button
-                key={index}
-                onClick={() => handlePageChange(index + 1)}
-                style={{
-                  ...styles.pageButton,
-                  backgroundColor: currentPage === index + 1 ? '#555' : '#333',
-                  color: currentPage === index + 1 ? '#fff' : '#bbb',
-                }}
-              >
-                {index + 1}
-              </button>
-            ))}
-          </div>
-          <button
+        <Box
+          sx={{
+            marginTop: '20px',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          {Array.from({ length: totalPages }, (_, index) => (
+           <Button
+           key={index}
+           onClick={() => handlePageChange(index + 1)}
+           sx={{
+             margin: '0 5px',
+             border: 'none',
+             borderRadius: '50%',
+             cursor: 'pointer',
+             transition: 'background-color 0.3s',
+             width: '36px', 
+             height: '36px',
+             padding: 0,
+             display: 'flex',
+             alignItems: 'center',
+             justifyContent: 'center',
+             backgroundColor: currentPage === index + 1 ? '#555' : '#333',
+             color: currentPage === index + 1 ? '#fff' : '#bbb',
+             minWidth: '0', 
+           }}
+         >
+           {index + 1}
+         </Button>
+         
+          
+           
+             
+          ))}
+          <Button
             onClick={handleNextPage}
-            disabled={isNextButtonDisabled}
-            style={{
-              ...styles.nextButton,
-              backgroundColor: isNextButtonDisabled ? '#999' : '#28a745',
-              cursor: isNextButtonDisabled ? 'not-allowed' : 'pointer',
+            disabled={currentPage === totalPages}
+            sx={{
+              marginLeft: '10px',
+              padding: '10px 15px',
+              border: 'none',
+              borderRadius: '5px',
+              color: '#fff',
+              transition: 'background-color 0.3s',
+              backgroundColor: currentPage === totalPages ? '#999' : '#28a745',
             }}
           >
             Next
-          </button>
-        </div>
+          </Button>
+        </Box>
       )}
-    </div>
+    </Box>
   );
 };
-
-const styles: { [key: string]: React.CSSProperties } = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: '#1E1E1E',
-    paddingBottom: '20px',
-    fontFamily: 'Poppins , sans-serif',
-  },
-  searchContainer: {
-    width: '70%',
-    height:"134px",
-    paddingTop:"33px",
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems:"center",
-    marginBottom: '30px',
-    marginTop:"30px",
-    border:"1.5px solid #252525",
-    position:"relative",
-    borderRadius:"4px"
-  },
-  searchInput: {
-    width: '60%',
-    padding: '10px',
-    borderRadius: '5px',
-    border: '1px solid #252525',
-    fontSize: '16px',
-    background:"#1E1E1E"
-  },
-  cardContainer: {
-    
-    width: '100%',
-    display: 'flex',
-    justifyContent:"space-around",
-    flexWrap: 'wrap',
-  
-    margin: '0 auto',
-   
-  },
-  card: {
-    width: '48%',
-    marginBottom: '20px',
-    
-  },
-  pagination: {
-    marginTop: '20px',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  nextButton: {
-    marginLeft: '10px',
-    padding: '10px 15px',
-    border: 'none',
-    borderRadius: '5px',
-    color: '#fff',
-    transition: 'background-color 0.3s',
-  },
-  pageNumbers: {
-    display: 'flex',
-    gap: '5px',
-  },
-  pageButton: {
-    margin: '0 5px',
-    padding: '5px 10px',
-    border: 'none',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s',
-    width: '36px',
-    height: '36px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-};
-
 
 export default Projects;

b98071acd80e9ed0e33f33159d919c7d9e65acb2
sumanpatra006 <ksumanpatra06@gmail.com>
2025-01-02 08:20:18 +0530

    update authschema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import '@fontsource/poppins/400.css'; 
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css'; 
+// import '@fontsource/poppins/400.css'; 
+// import '@fontsource/poppins/500.css';
+// import '@fontsource/poppins/600.css';
+// import '@fontsource/poppins/700.css'; 
 import ResponsiveAppBar from "@/components/navbar/ResponsiveAppBar";
 import Footer from "@/components/footer/Footer";
 

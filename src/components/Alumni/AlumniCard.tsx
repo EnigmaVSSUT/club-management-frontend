fdd58d2a0196e2534929319a7609f534c483c849
sumanpatra006 <ksumanpatra06@gmail.com>
2025-01-03 09:22:23 +0530

    feat:change font size for phone view & update font family for some text

--- src/components/Alumni/AlumniCard.tsx
+++ src/components/Alumni/AlumniCard.tsx
@@ -18,15 +18,15 @@ const AlumniCard: React.FC<AboutProps> = ({ Img_Url, Name, Branch, PassOutYear, 
                     <AlumniPfp imgUrl={Img_Url}/>
                 </div>
         
-                <div className='flex justify-center text-center text-[1.8vw] font-semibold text-[#FFFFFF] pt-[2vw]'>
+                <div className='flex justify-center text-center text-[3.1vw] sm:text-[1.8vw] font-semibold text-[#FFFFFF] pt-[2vw]'>
                     {Name}
                 </div>
         
-                <div className='flex justify-center text-center text-[0.9vw] text-[#BDBDBD]'>
+                <div className='flex justify-center text-center text-[2vw] sm:text-[0.9vw] text-[#BDBDBD]'>
                     {Branch},&nbsp;{PassOutYear}
                 </div>
         
-                <div className='flex justify-center text-center text-[1.2vw] text-[#BDBDBD] pt-[2vw]'>
+                <div className='flex justify-center text-center text-[2.4vw] sm:text-[1.2vw] text-[#BDBDBD] pt-[2vw]'>
                     {AboutMe}
                 </div>
         
